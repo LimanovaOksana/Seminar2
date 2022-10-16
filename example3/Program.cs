d3aaa2788909866884d50017a8baae641cc8a004
Oksana <okslim1992@gmail.com>
2022-10-16 23:57:09 +0700

    ex3

---
+++ example3/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 11, 21, 31, 18, 4, 15, 61, 17, 18 };
+
+int n = array.Length;
+int find = 18;
+int index = 0;
+while (index < n)
+{
+    if(array[index] == find)
+    
+    {
+        Console.WriteLine(index);
+        break; // чтобы найти самый первый элемент в массиве
+    }
+    index++;
+}        
+
